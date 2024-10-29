fbf46e94b2036820738cfab578808f6fa4f24a95
unknown <wesleyliam005@gmail.com>
2024-10-28 17:35:02 -0700

    added product filter logic

---
+++ MPESA/stk_push.py
@@ -0,0 +1,7 @@
+# import requests
+
+# ​
+
+# response = requests.request("GET", 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', headers = { 'Authorization': 'Basic MTdlQUEwODJ0SVh0WHV4cmVWZHhmVGg3V0RHZkxJRVdxWERIRzRaRjZRaDFJUHFUOmFDTmNkdU82WGI0WU5DMzBCQVlRekdZcG5jbWFvMGdLYjlWT2U4Q1o0N0pNck9XTlRVcFBjZ0laZGNHN0JmcUw=' })
+
+# print(response.text.encode('utf8'))
